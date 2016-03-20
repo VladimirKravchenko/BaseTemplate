35e64430efcf6e0004ef090ad14cea767efa4d2e
Vladimir Kravchenko <vladimir@Vladimirs-iMac.local>
2016-03-20 09:50:07 +0200

    pods

---
+++ BaseTemplateExample/BaseTemplateExample/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BaseTemplateExample
+//
+//  Created by Vladimir Kravchenko on 3/20/16.
+//  Copyright © 2016 Vladimir Kravchenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    
+    return true
+  }
+
+}
+
